14ae85beca74064f7ebbc3e29c29d79fc2672f6d
Tan Chang Kiat <tanchangkiat@gmail.com>
2023-01-20 00:11:14 +0800

    Add a new Stage in API Gateway

--- aws_cdk_serverless/aws_cdk_serverless_stack.py
+++ aws_cdk_serverless/aws_cdk_serverless_stack.py
@@ -1,8 +1,9 @@
 from aws_cdk import (
     # Duration,
     Stack,
-    aws_lambda as _lambda,
     aws_apigateway as apigw,
+    aws_iam as iam,
+    aws_lambda as _lambda,
     aws_logs as logs,
 )
 from constructs import Construct
@@ -25,6 +26,7 @@ class AwsCdkServerlessStack(Stack):
             handler='hello.handler',
             function_name=app_name+'-hello',
         )
+        fn_hello.grant_invoke(iam.ServicePrincipal('apigateway.amazonaws.com'))
 
         # Handles '/products'
         fn_products = _lambda.Function(self, "function_products",
@@ -33,17 +35,18 @@ class AwsCdkServerlessStack(Stack):
             handler='products.handler',
             function_name=app_name+'-products',
         )
+        fn_products.grant_invoke(iam.ServicePrincipal('apigateway.amazonaws.com'))
 
         # ------------------------------------
         # API Gateway
         # ------------------------------------
 
         # Creates a REST API in API Gateway
-        apigw_logs = logs.LogGroup(self, "api_gateway_logs", log_group_name=app_name+"-api-gateway-logs", retention=logs.RetentionDays.THREE_DAYS)
+        apigw_logs_v1 = logs.LogGroup(self, "api_gateway_logs_v1", log_group_name=app_name+"-v1-api-gateway-logs", retention=logs.RetentionDays.THREE_DAYS)
         api = apigw.RestApi(
             self, 'api',
             deploy_options=apigw.StageOptions(
-                access_log_destination=apigw.LogGroupLogDestination(apigw_logs),
+                access_log_destination=apigw.LogGroupLogDestination(apigw_logs_v1),
                 access_log_format=apigw.AccessLogFormat.json_with_standard_fields(
                     caller=False,
                     http_method=True,
@@ -55,10 +58,12 @@ class AwsCdkServerlessStack(Stack):
                     status=True,
                     user=True
                 ),
+                logging_level=apigw.MethodLoggingLevel.INFO,
                 stage_name='v1'
             ),
             rest_api_name=app_name,
         )
+        deployment = apigw.Deployment(self, "Deployment", api=api)
 
         # Lambda Integration for '/'
         integration_hello = apigw.LambdaIntegration(fn_hello)
@@ -68,3 +73,22 @@ class AwsCdkServerlessStack(Stack):
         integration_products = apigw.LambdaIntegration(fn_products)
         products = api.root.add_resource("products")
         products.add_method("GET", integration_products)
+
+        apigw_logs_v2 = logs.LogGroup(self, "api_gateway_logs_v2", log_group_name=app_name+"-v2-api-gateway-logs", retention=logs.RetentionDays.THREE_DAYS)
+        stage_v2 = apigw.Stage(self, "stage-v2",
+            deployment=deployment,
+            access_log_destination=apigw.LogGroupLogDestination(apigw_logs_v2),
+            access_log_format=apigw.AccessLogFormat.json_with_standard_fields(
+                caller=False,
+                http_method=True,
+                ip=True,
+                protocol=True,
+                request_time=True,
+                resource_path=True,
+                response_length=True,
+                status=True,
+                user=True
+            ),
+            logging_level=apigw.MethodLoggingLevel.INFO,
+            stage_name='v2'
+        )
