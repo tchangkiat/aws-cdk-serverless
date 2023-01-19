861cdc131ce08708d93f02a6e268d4c9d0591c8e
Tan Chang Kiat <tanchangkiat@gmail.com>
2023-01-19 18:28:54 +0800

    Added a sample Lambda function

--- aws_cdk_serverless/aws_cdk_serverless_stack.py
+++ aws_cdk_serverless/aws_cdk_serverless_stack.py
@@ -1,7 +1,7 @@
 from aws_cdk import (
     # Duration,
     Stack,
-    # aws_sqs as sqs,
+    aws_lambda as _lambda,
 )
 from constructs import Construct
 
@@ -10,10 +10,9 @@ class AwsCdkServerlessStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
-
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "AwsCdkServerlessQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        _lambda.Function(self, "function_hello",
+            runtime=_lambda.Runtime.PYTHON_3_9,
+            code=_lambda.Code.from_asset('lambda_functions'),
+            handler='hello.handler',
+            function_name='hello',
+        )
