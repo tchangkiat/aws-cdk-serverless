51a004d1070de064d4e58c957ca8f67f0ac0e5f9
Tan Chang Kiat <tanchangkiat@gmail.com>
2023-01-20 15:44:08 +0800

    Added API Gateway Stages and refactor code

--- stacks/serverless.py
+++ stacks/serverless.py
@@ -19,18 +19,20 @@ class ApiGatewayLambda(Stack):
 
         # Handles '/'
         fn_hello = _lambda.Function(self, "function_hello",
-            runtime=_lambda.Runtime.PYTHON_3_9,
             code=_lambda.Code.from_asset('lambda_functions'),
-            handler='hello.handler',
             function_name=app_name+'-hello',
+            handler='hello.handler',
+            log_retention=logs.RetentionDays.THREE_DAYS,
+            runtime=_lambda.Runtime.PYTHON_3_9,
         )
 
         # Handles '/products'
         fn_products = _lambda.Function(self, "function_products",
-            runtime=_lambda.Runtime.PYTHON_3_9,
             code=_lambda.Code.from_asset('lambda_functions'),
-            handler='products.handler',
             function_name=app_name+'-products',
+            handler='products.handler',
+            log_retention=logs.RetentionDays.THREE_DAYS,
+            runtime=_lambda.Runtime.PYTHON_3_9,
         )
 
         # ------------------------------------
@@ -38,25 +40,44 @@ class ApiGatewayLambda(Stack):
         # ------------------------------------
 
         # Creates a REST API in API Gateway
-        apigw_logs = logs.LogGroup(self, "api_gateway_logs", log_group_name=app_name+"-api-gateway-logs", retention=logs.RetentionDays.THREE_DAYS)
         api = apigw.RestApi(
             self, 'api',
-            deploy_options=apigw.StageOptions(
-                access_log_destination=apigw.LogGroupLogDestination(apigw_logs),
-                access_log_format=apigw.AccessLogFormat.json_with_standard_fields(
-                    caller=False,
-                    http_method=True,
-                    ip=True,
-                    protocol=True,
-                    request_time=True,
-                    resource_path=True,
-                    response_length=True,
-                    status=True,
-                    user=True
-                ),
-                logging_level=apigw.MethodLoggingLevel.INFO,
-            ),
+            deploy=False,
             rest_api_name=app_name,
+        )
+
+        deployment_v1 = apigw.Deployment(self, "deployment_v1", api=api)
+        deployment_v2 = apigw.Deployment(self, "deployment_v2", api=api)
+
+        logs_v1 = logs.LogGroup(self, "logs_v1", log_group_name=app_name+"-v1-api-gateway-logs", retention=logs.RetentionDays.THREE_DAYS)
+        logs_v2 = logs.LogGroup(self, "logs_v2", log_group_name=app_name+"-v2-api-gateway-logs", retention=logs.RetentionDays.THREE_DAYS)
+
+        log_format = apigw.AccessLogFormat.json_with_standard_fields(
+            caller=False,
+            http_method=True,
+            ip=True,
+            protocol=True,
+            request_time=True,
+            resource_path=True,
+            response_length=True,
+            status=True,
+            user=True
+        )
+
+        apigw.Stage(self, "stage_v1",
+            deployment=deployment_v1,
+            access_log_destination=apigw.LogGroupLogDestination(logs_v1),
+            access_log_format=log_format,
+            logging_level=apigw.MethodLoggingLevel.INFO,
+            stage_name='v1'
+        )
+
+        apigw.Stage(self, "stage_v2",
+            deployment=deployment_v2,
+            access_log_destination=apigw.LogGroupLogDestination(logs_v2),
+            access_log_format=log_format,
+            logging_level=apigw.MethodLoggingLevel.INFO,
+            stage_name='v2'
         )
 
         # Lambda Integration for '/'
