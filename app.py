258abb0e533bf08dc3c945f4160705c5b62730a9
Tan Chang Kiat <tanchangkiat@gmail.com>
2023-01-20 11:59:30 +0800

    Refactor code to improve code quality

--- app.py
+++ app.py
@@ -3,11 +3,11 @@ import os
 
 import aws_cdk as cdk
 
-from aws_cdk_serverless.serverless_stack import ServerlessStack
+from stacks.serverless import Serverless
 
 
 app = cdk.App()
-ServerlessStack(app, "cdk-serverless-app",
+Serverless(app, "cdk-serverless-app",
     env=cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
 )
 
